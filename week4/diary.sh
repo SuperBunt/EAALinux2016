1b7f5ea41610c8fcedbf5d6e127a161a34ff767e
Peter Connell <peterdconnell@hotmail.com>
2016-10-14 10:01:59 +0100

    week 4 diary lab

---
+++ week4/diary.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# >> redirect command
+#diary.txt is a text file
+# $(cmd) execute the command
+
+read text
+
+echo 'date' $text >> diary.txt
+
+#alternative solution
+
+#read diaryEntry
+
+#var d=`date +%F -%r`
+
+#echo $d $1 >> diary.txt
+
+#echo "entry appended"
